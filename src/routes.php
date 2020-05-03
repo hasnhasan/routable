de302436f9e77603363f3d400ea84e6ea308e342
GoldSteam <goldsteam@livegames.io>
2020-05-03 03:54:27 +0300

    Update routes.php

--- src/routes.php
+++ src/routes.php
@@ -1,9 +1,32 @@
 <?php
 
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Request;
 use Illuminate\Support\Facades\Route;
 
 $routableModel = config('routable.routableModel');
+
+Route::bind('route', function ($value, $route) use ($routableModel) {
+    $modelFolder = config('routable.modelFolder', 'Models');
+
+    $model = ucfirst($route->bindingFieldFor('route'));
+    $model = collect(File::allFiles(app_path($modelFolder)))->map(function ($class) {
+        $classNamespace = str_replace([app_path(), '/', '.php'], ['App', '\\', ''], $class->getRealPath());
+        $className      = class_basename($classNamespace);
+
+        return (object) [
+            'model'      => $classNamespace,
+            'model_name' => $className,
+        ];
+    })->where('model_name', $model)->first();
+
+    $route = $routableModel::where('slug', $value)->when($model, function ($q, $model) {
+        return $q->where('routable_type', $model->model);
+    })->firstOrFail();
+
+    return $route->routable;
+});
+
 if ($routeData = $routableModel::where('slug', Request::path())->first()) {
     $middleware = (array) config('routable.middleware');
     $action     = $routeData->uses;
